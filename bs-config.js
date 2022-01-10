d358b8e9f768ac7b72dde800ec873423002da5b4
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-01-10 17:58:09 -0500

    Clase 09: Componentes dinamicos

--- bs-config.js
+++ bs-config.js
@@ -2,7 +2,7 @@ module.exports = {
     server: {
         middleware: {
             1: require('connect-history-api-fallback')({
-                index: './clases/08v-model-componentes.html',
+                index: './clases/09componentes-dinamicos.html',
                 verbose: true,
             })
         }
